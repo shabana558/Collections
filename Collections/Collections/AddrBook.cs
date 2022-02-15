58d874bbc4e20d69113e4f7fa9dc24fcc25d06f1
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-15 15:24:44 +0530

    [shabana]Duplicateentries

---
+++ Collections/Collections/AddrBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    public class AddrBook
+    {
+        internal List<AddrBook> people;
+
+        internal void ListingPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void GetCustomer(string firstName, string lastName, string phoneNumber, string addresses, string city, string state, string zipCode, string emailId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
